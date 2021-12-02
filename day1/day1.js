869a45b6722fe8c9983ba7a52f0b9d05b7ecaa27
Bronwyn Taljaard <bronwyn.greeff@gmail.com>
2021-12-02 11:38:37 +0100

    adding day 1

---
+++ day1/day1.js
@@ -0,0 +1,43 @@
+fs = require("fs");
+
+let testInput = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+let day1Input = dayInputFromFile("day1In.txt");
+
+function dayInputFromFile(fileName) {
+  let fileContent = fs.readFileSync(fileName, { encoding: "utf-8" });
+  return fileContent.split("\n").map((num) => parseInt(num));
+}
+
+function findNumberOfMeasurementIncreases(measurements) {
+  let counter = 0;
+  for (let i = 1; i < measurements.length; i++) {
+    if (measurements[i] - measurements[i - 1] > 0) {
+      counter += 1;
+    }
+  }
+  return counter;
+}
+
+const res = findNumberOfMeasurementIncreases(day1Input);
+// console.log(res);
+
+// PART TWO
+
+function findNumberOfMeasurementIncreasesSliding(measurements) {
+  let counter = 0;
+  let previousSliding = measurements[0] + measurements[1] + measurements[2];
+  let currentSliding = 0;
+  for (let i = 1; i < measurements.length; i++) {
+    if (i < measurements.length - 2) {
+      currentSliding =
+        measurements[i] + measurements[i + 1] + measurements[i + 2];
+      if (currentSliding > previousSliding) {
+        counter += 1;
+      }
+      previousSliding = currentSliding;
+    }
+  }
+  return counter;
+}
+
+console.log(findNumberOfMeasurementIncreasesSliding(day1Input));
